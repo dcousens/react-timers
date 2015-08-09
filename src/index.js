e4e98b9b050a898aeac32e7674b0e95d9961c318
Daniel Cousens <github@dcousens.com>
2015-08-09 15:43:13 +1000

    ignore setImmediate

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
 function clearTimers () {
-  this.clearImmediates()
   this.clearIntervals()
   this.clearTimeouts()
 }
 
 module.exports = function Timers () {
-  let immediates, intervals, timeouts
+  let intervals, timeouts
 
   return {
-    clearImmediates () { immediates.forEach(clearImmediate) },
     clearIntervals () { intervals.forEach(clearInterval) },
     clearTimeouts () { timeouts.forEach(clearTimeout) },
-    clearImmediate (... args) { return clearImmediate(... args) },
     clearInterval (... args) { return clearInterval(... args) },
     clearTimeout (... args) { return clearTimeout(... args) },
     clearTimers,
 
     componentWillMount () {
-      immediates = []
       intervals = []
       timeouts = []
     },
     componentWillUnmount: clearTimers,
 
-    setImmediate (callback, ... args) {
-      return immediates[immediates.push(setImmediate((... params) => { callback.call(this, ... params) }, ... args)) - 1]
-    },
     setInterval (callback, ... args) {
       return intervals[intervals.push(setInterval((... params) => { callback.call(this, ... params) }, ... args)) - 1]
     },
